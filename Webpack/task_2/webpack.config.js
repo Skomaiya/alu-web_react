2865ee9d838a0ed461474c454cc1fb378107e93a
Skomaiya <s.komaiya@alustudent.com>
2025-09-10 15:27:21 +0200

    Complete Task 3 and run code build

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', 
+
+    entry: './js/dashboard_main.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
